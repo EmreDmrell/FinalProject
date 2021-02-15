e943214dbf115cc76c2a6991b532a610372817b6
Emre DEMİREL <19060364@stu.omu.edu.tr>
2021-02-15 20:00:44 +0300

    15 Şubat

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Business;
+using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
@@ -9,6 +10,43 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // ProductTest();
+
+            // UnitPriceTest();
+
+            // CategoryTest();
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var p in productManager.GetProductDetails())
+            {
+                Console.WriteLine($"{p.ProductName} / {p.CategoryName} ");
+            }
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void UnitPriceTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());            
+
+            foreach (var p in productManager.GetByUnitPrice(40, 100))
+            {
+                Console.WriteLine("{0} : {1}$", p.ProductName, p.UnitPrice);
+            }
+        }
+
+        private static ProductManager ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
             foreach (var product in productManager.GetAllByCategoryId(2))
@@ -16,14 +54,7 @@ namespace ConsoleUI
                 Console.WriteLine(product.ProductName);
             }
 
-            Console.WriteLine("--------------------------");
-
-            foreach (var p in productManager.GetByUnitPrice(40, 100))
-            {
-                Console.WriteLine("{0} : {1}$", p.ProductName, p.UnitPrice);
-            }
-
-            
+            return productManager;
         }
     }
 }
