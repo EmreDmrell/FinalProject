0263771e1a03f8f0e80be6462015d2c92944399e
Emre DEMİREL <19060364@stu.omu.edu.tr>
2021-02-15 20:00:30 +0300

    15 Şubat

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            Console.WriteLine("--------------------------");
+
+            foreach (var p in productManager.GetByUnitPrice(40, 100))
+            {
+                Console.WriteLine("{0} : {1}$", p.ProductName, p.UnitPrice);
+            }
+
+            
+        }
+    }
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    // generic constraint   
+    // class : Buradaki Class referans tipi temsil eder
+    // IEntity : IEntity de olabilir Ientity implemente eden class da olabilir
+    // new() : New lenebilir olmalı
+    public interface IEntityRepository<T> where T: class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);       
+    }
+}
